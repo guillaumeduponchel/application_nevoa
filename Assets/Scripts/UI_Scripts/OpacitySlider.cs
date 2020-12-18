8c3e9bdaedc0098988cf2059de3279243bc92f43
guillaumeduponchel <43133513+guillaumeduponchel@users.noreply.github.com>
2020-12-19 00:37:18 +0100

    Add slider linked to opacity shader

---
+++ Assets/Scripts/UI_Scripts/OpacitySlider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpacitySlider : MonoBehaviour
+{
+
+    public Slider opacitySliderObject;
+    public Material VPMaterial;
+    
+    void Start()
+    {
+        opacitySliderObject.onValueChanged.AddListener (delegate {ValueChangeCheck ();});
+    }
+
+    public void ValueChangeCheck()
+	{
+		Debug.Log (opacitySliderObject.value);
+        VPMaterial.SetFloat("OPACITY_VECTOR", opacitySliderObject.value);
+	}
+}
