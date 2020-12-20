8d3aca46b75d37d13ee582a50cfce77aa6672224
guillaumeduponchel <43133513+guillaumeduponchel@users.noreply.github.com>
2020-12-20 18:11:28 +0100

    Add dropdown for blendmodes

---
+++ Assets/Scripts/UI_Scripts/BlendingModeDropdown.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BlendingModeDropdown : MonoBehaviour
+{
+
+    public Dropdown blendingModeDropDown;
+
+    public Material matVP;
+
+    private int curEnum = 0;
+    private string[] EnumTable = {"BLENDING_ENUM_SCREEN", "BLENDING_ENUM_DIFFERENCE", "BLENDING_ENUM_MULTIPLY", "BLENDING_ENUM_OVERWRITE"};
+    void Start()
+    {
+        foreach (string blendMode in EnumTable)
+        {
+            matVP.DisableKeyword(blendMode);
+        }
+        matVP.EnableKeyword(EnumTable[curEnum]);
+        matVP.SetFloat("BLENDING_ENUM", 0);
+        blendingModeDropDown.onValueChanged.AddListener (delegate {ValueChangeCheck ();});
+    }
+
+    public void ValueChangeCheck()
+	{
+        int newEnum = blendingModeDropDown.value;
+        matVP.DisableKeyword(EnumTable[curEnum]);
+        matVP.EnableKeyword(EnumTable[newEnum]); 
+        matVP.SetFloat("BLENDING_ENUM", newEnum);  
+        curEnum = newEnum;
+	}
+}
